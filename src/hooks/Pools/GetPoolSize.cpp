1f7c499f1e7aa9b654b1fbadb2a29e01c84e8a78
maybegreat48 <email@hostname>
2023-12-10 11:57:30 -0500

    Some pool tweaks?

--- src/hooks/Pools/GetPoolSize.cpp
+++ src/hooks/Pools/GetPoolSize.cpp
@@ -5,21 +5,21 @@
 namespace NewBase
 {
 	static const std::unordered_map<uint32_t, unsigned int> s_PoolSizeOverrides = {
-	    {"CEventNetwork"_J, 5000},      //  // allow 10x events to be stored
-	    {"Decorator"_J, 1400},          //      // try to prevent decorator crashes
-	    {"FragmentStore"_J, 50000},    //  // .yft files
-	    {"fwScriptGuid"_J, 3072},       //     // increase script entity handles
-	    {"HandlingData"_J, 5100},       //     // handling files
-	    {"CPickupPlacement"_J, 280},    //     // map pickups
-	    {"CEvent"_J, 4200},             //  // AI events, increasing this apparently fixes some crashes
-	    {"CTaskSequenceList"_J, 250},   //  // task sequences, see above
+	    {"CEventNetwork"_J, 5000},      // allow 10x events to be stored
+	    {"Decorator"_J, 1400},          // try to prevent decorator crashes
+	    {"FragmentStore"_J, 50000},     // .yft files
+	    {"fwScriptGuid"_J, 3072},       // increase script entity handles
+	    {"HandlingData"_J, 5100},       // handling files
+	    {"CPickupPlacement"_J, 280},    // map pickups
+	    {"CEvent"_J, 4200},             // AI events, increasing this apparently fixes some crashes
+	    {"CTaskSequenceList"_J, 250},   // task sequences, see above
 	    {"AnimatedBuilding"_J, 1200},   //
 	    {"Building"_J, 66000},          //
-	    {"CCombatInfo"_J, 100},         //  // required by some mods?
-	    {"ScenarioPoint"_J, 1800},      //  // map mods
-	    {"ScenarioPointEntity"_J, 700}, //  // map mods
-	    {"ScenarioPointWorld"_J, 2800}, //  // map mods
-	    {"MaxNonRegionScenarioPointSpatialObjects"_J, 1800}, //  // map mods
+	    {"CCombatInfo"_J, 100},         // required by some mods?
+	    {"ScenarioPoint"_J, 1800},      // map mods
+	    {"ScenarioPointEntity"_J, 700}, // map mods
+	    {"ScenarioPointWorld"_J, 2800}, // map mods
+	    {"MaxNonRegionScenarioPointSpatialObjects"_J, 1800}, // map mods
 	    {"Object"_J, 3000},                                  //
 	};
 
